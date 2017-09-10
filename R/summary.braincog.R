a48ae8c1b34c609e4413d3b16ecc80a3a7a7ce7b
Christof Seiler <christof.seiler@gmail.com>
2017-09-10 11:52:22 -0700

    fix color and bug when no clusters are signficant

--- R/summary.braincog.R
+++ R/summary.braincog.R
@@ -10,15 +10,15 @@ summary.braincog = function(fit) {
   # extract from results
   pvalues = fit$pvalues
   cs_perm = fit$cs_perm
-  
+
   # cluster colors
-  color = brewer.pal(n = 9, name = "Set1")[1:length(pvalues)]
+  color = brewer.pal(n = 8, name = "Set1")[seq(pvalues)]
   color[is.na(color)] = "black"
 
   # make table to summarize everyting
-  tibble(id = 1:length(pvalues),
-         label = 2:(length(pvalues)+1), # offset by one for background
-         size = cs_perm[1,1:length(pvalues)],
+  tibble(id = seq(pvalues),
+         label = seq(pvalues)+1, # offset by one for background
+         size = cs_perm[1,seq(pvalues)],
          color = color,
          pvalue = pvalues,
          pvalue_adj = pvalues %>%
